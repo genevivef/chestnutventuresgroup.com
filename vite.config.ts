c9f11354114c2a667b1346145c89a85b2a000bca
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2025-07-09 10:46:25 -0400

    Add enhanced img plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
+import { enhancedImages } from "@sveltejs/enhanced-img";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
+		enhancedImages(),
 		sveltekit(),
 	],
 });
