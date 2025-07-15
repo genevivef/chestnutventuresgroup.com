3f7143b0a364a29f5001bb39753aa52b1660c376
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2025-07-15 11:54:53 -0400

    Improve mobile readability

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,15 +6,15 @@
     import * as Carousel from "$lib/components/ui/carousel";
 </script>
 
-<div class="w-full flex flex-col items-center gap-8 bg-linear-to-b from-emerald-300 from-50% p-32">
-    <h1 class="text-6xl font-semibold text-center">Backing builders of what's next.</h1>
-    <p class="text-2xl text-center">We're an early-stage venture firm that leads Pre-Seed, Seed and Series A rounds in standout software companies.</p>
-    <Button class="h-12 text-xl" size="lg" href="#about">
+<div class="w-full flex flex-col items-center gap-8 bg-linear-to-b from-emerald-300 from-50% px-8 py-16 md:p-32">
+    <h1 class="text-4xl md:text-6xl font-semibold text-center">Backing builders of what's next.</h1>
+    <p class="text-lg md:text-2xl text-center">We're an early-stage venture firm that leads Pre-Seed, Seed and Series A rounds in standout software companies.</p>
+    <Button class="h-12 text-lg md:text-xl" size="lg" href="#about">
         Learn more
     </Button>
 </div>
 
-<div class="flex flex-col md:flex-row md:items-center gap-32 px-16">
+<div class="flex flex-col md:flex-row md:items-center gap-16 lg:gap-32 px-8 md:px-16">
     <div class="md:basis-1/2 flex flex-col gap-8">
         <h3 class="text-4xl font-semibold">Investing across frontiers</h3>
         <p class="text-lg mb-8">From AI and fintech to vertical SaaS and infrastructure, we partner with founders turning ambitious ideas into category-defining businesses.</p>
@@ -23,13 +23,13 @@
         <p class="text-lg">We back founders early and help them move faster. Whether it's building the first team, landing early customers, or shaping product strategy, we're here to support the journey—without getting in the way.</p>
     </div>
 
-    <div class="md:basis-1/2 flex flex-col gap-32">
+    <div class="md:basis-1/2 flex flex-col gap-16 md:gap-32">
         <Card.Root>
             <Card.Header>
                 <Card.Title class="text-2xl">Sectors</Card.Title>
             </Card.Header>
             <Card.Content>
-                <div class="flex gap-4">
+                <div class="grid grid-cols-2 xl:flex gap-4">
                     <Badge variant="secondary" class="text-lg">AI</Badge>
                     <Badge variant="secondary" class="text-lg">Fintech</Badge>
                     <Badge variant="secondary" class="text-lg">Vertical SaaS</Badge>
@@ -43,7 +43,7 @@
                 <Card.Title class="text-2xl">Investment focus</Card.Title>
             </Card.Header>
             <Card.Content>
-                <div class="flex gap-4">
+                <div class="grid grid-cols-2 xl:flex gap-4">
                     <Badge class="text-lg">Pre-seed</Badge>
                     <Badge class="text-lg">Seed</Badge>
                     <Badge class="text-lg">Series A</Badge>
@@ -55,7 +55,7 @@
 
 <Separator />
 
-<div class="flex flex-col gap-8 px-16">
+<div class="flex flex-col gap-8 px-8 md:px-16">
     <h2 class="text-2xl font-semibold">Our team</h2>
 
     <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
@@ -99,7 +99,7 @@
 
 <Separator />
 
-<div class="px-16">
+<div class="px-8 md:px-16">
     <Carousel.Root>
         <div class="flex justify-between pb-8">
             <h2 class="text-2xl font-semibold">Portfolio</h2>
@@ -110,27 +110,27 @@
         </div>
 
         <Carousel.Content>
-            <Carousel.Item class="basis-1/2 md:basis-1/3">
+            <Carousel.Item class="basis-full md:basis-1/3">
                 <enhanced:img class="rounded-lg" src="$lib/assets/portfolio/deepen.png" alt="Deepen.AI" />
                 <p class="pt-4">Deepen AI</p>
             </Carousel.Item>
-            <Carousel.Item class="basis-1/2 md:basis-1/3">
+            <Carousel.Item class="basis-full md:basis-1/3">
                 <enhanced:img class="rounded-lg" src="$lib/assets/portfolio/transastra.png" alt="TransAstra" />
                 <p class="pt-4">TransAstra</p>
             </Carousel.Item>
-            <Carousel.Item class="basis-1/2 md:basis-1/3">
+            <Carousel.Item class="basis-full md:basis-1/3">
                 <enhanced:img class="rounded-lg" src="$lib/assets/portfolio/dor.png" alt="Dor" />
                 <p class="pt-4">Dor</p>
             </Carousel.Item>
-            <Carousel.Item class="basis-1/2 md:basis-1/3">
+            <Carousel.Item class="basis-full md:basis-1/3">
                 <enhanced:img class="rounded-lg" src="$lib/assets/portfolio/leanplum.png" alt="Leanplum" />
                 <p class="pt-4">Leanplum</p>
             </Carousel.Item>
-            <Carousel.Item class="basis-1/2 md:basis-1/3">
+            <Carousel.Item class="basis-full md:basis-1/3">
                 <enhanced:img class="rounded-lg" src="$lib/assets/portfolio/saga.png" alt="Saga" />
                 <p class="pt-4">Saga</p>
             </Carousel.Item>
-            <Carousel.Item class="basis-1/2 md:basis-1/3">
+            <Carousel.Item class="basis-full md:basis-1/3">
                 <enhanced:img class="rounded-lg" src="$lib/assets/portfolio/pathcision.png" alt="PathCision Medicine" />
                 <p class="pt-4">PathCision Medicine</p>
             </Carousel.Item>
