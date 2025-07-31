8f3784d876cc3e1cc8952764edd9c5ac1ff7481d
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2025-07-31 17:33:07 +0200

    Add sonner component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import "../app.css";
 	import Header from "./Header.svelte";
+	import { Toaster } from "$lib/components/ui/sonner";
 
 	let { children } = $props();
 </script>
+
+<Toaster richColors />
 
 <Header />
 
