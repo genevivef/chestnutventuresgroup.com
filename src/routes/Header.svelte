ce7a21f805a063ad61172420f2a23c0ffb0cb611
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2025-08-01 23:44:30 +0200

    Fix layout in header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -4,7 +4,7 @@
     import { smoothScroll } from "$lib/utils";
 </script>
 
-<header class="flex flex-row items-center justify-between p-4 md:p-6">
+<header class="w-full flex flex-row items-center justify-between p-4 md:p-6">
     <a class="flex flex-row items-center gap-4" href="/" aria-label="Home">
         <enhanced:img class="max-h-10 md:max-h-12 w-auto rounded-lg" src="$lib/assets/cvg-logo.png" alt="Chestnut Ventures Group Logo" />
         <span class="text-lg md:text-2xl font-semibold">Chestnut Ventures Group</span>
