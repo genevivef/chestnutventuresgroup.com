241d821f36450398bb9f5277eb6827a7804a11f3
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2025-08-03 11:03:50 +0200

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { page } from "$app/state";
+    import { Button } from "$lib/components/ui/button";
+</script>
+
+<div class="flex flex-col items-center justify-center gap-8 py-16">
+    <h1 class="text-6xl text-center text-primary font-semibold">{page.status}</h1>
+    <h2 class="text-4xl text-center">{page.error?.message}</h2>
+    <Button class="text-lg" size="lg" href="/">
+        Go Home
+    </Button>
+</div>
